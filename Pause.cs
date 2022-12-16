3c7b8e8a1a75f0b173d922fed284dee9545957e4
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2022-12-15 21:17:21 -0800

    Added Name

--- Pause.cs
+++ Pause.cs
@@ -5,8 +5,12 @@ namespace R3peat
     class Pause: IAction
     {
         private int Delay;
+        private string Name;
         public void Run() {
             Thread.Sleep(this.Delay);
+        }
+        public string GetName() {
+            return this.Name;
         }
         public Pause(int milliseconds)
         {
