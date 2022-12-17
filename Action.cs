f01eb0840d213df95be4c4dc8155d0fa4c116ae7
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2022-12-16 16:35:35 -0800

    Changed Action Interface to Abstract class

---
+++ Action.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R3peat
+{
+    public abstract class Action
+    {
+        protected string Name;
+        public abstract void Run();
+        public String GetName() {
+            return this.Name;
+        }
+        public void SetName(String newName) {
+            this.Name=newName;
+        }
+
+    }
+}
