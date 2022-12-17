613ae72ef683ceaedf9a2173e3e80e9106f1806a
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2022-12-16 16:35:54 -0800

    Renamed window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,9 +18,9 @@ namespace R3peat
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MacroEditorWindow : Window
     {
-        public MainWindow()
+        public MacroEditorWindow()
         {
             InitializeComponent();
         }
