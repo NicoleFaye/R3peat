e286c503783b4f7a4315d903726f39c06a1ccd17
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2022-12-18 11:02:37 -0800

    Created a Pause builder to deal with names

---
+++ PauseBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using WindowsInput;
+
+namespace R3peat
+{
+    class PauseBuilder 
+    {
+        private int PauseDuration;
+        private Pause pause;
+        private string Name;
+        private int totalBuiltWithNoName { get; set; }
+        public void BuildPause() {
+            this.PauseDuration = 100;
+            this.totalBuiltWithNoName+=1;
+            this.Name = "Pause "+totalBuiltWithNoName.ToString();
+            this.pause = new Pause(this.PauseDuration,this.Name);
+        }
+        public void BuildPause(string Name) {
+            this.Name = Name; 
+        }
+        public void SetName(string Name) {
+            this.Name = Name;
+        }
+        public void SetPauseDuration(int PauseDuration) {
+            this.PauseDuration = PauseDuration;
+        }
+        public Pause GetPause() {
+            return this.pause;
+        }
+
+        public PauseBuilder() {
+        }
+
+    }
+}
