ba5705d6be69a70aad1a3039c058126c3af146aa
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2022-12-30 22:37:12 -0800

    Moved Name property to a new class

--- Action.cs
+++ Action.cs
@@ -8,24 +8,11 @@ using System.Threading.Tasks;
 
 namespace R3peat
 {
-    public abstract class Action :INotifyPropertyChanged
+    public abstract class Action : NameProperty
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-        private string _name;
-        public string Name { 
-            get {
-                return _name; 
-            }
-            set {
-                _name = value;
-                onPropertyChanged("Name");
-            }
-        }
+
         public abstract void Run();
-        private void onPropertyChanged(string propertyName){
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-                handler(this, new PropertyChangedEventArgs(propertyName));
-        }
+
+
     }
 }
---
+++ NameProperty.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R3peat
+{
+    public class NameProperty : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string _name = "";
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                onPropertyChanged("Name");
+            }
+        }
+        private void onPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
