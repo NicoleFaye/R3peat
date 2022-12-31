6bc60a6d0e40f63500ae5639f9d5f90e19c75838
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2022-12-30 22:38:16 -0800

    Create NameIncrementer.cs

---
+++ NameIncrementer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R3peat
+{
+    public class NameIncrementer 
+    {
+        private string _type;
+        public int count { get; set; }
+        public NameIncrementer(string type) {
+            _type = type;
+            count = 1;
+        }
+        public string Next() {
+            return _type + " " + count++.ToString();
+        }
+
+    }
+}
