be1da0e3d884fdf7c3174c05204e88f5e5feff7b
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2022-12-30 22:38:48 -0800

    Fixed bug and added Name incrementer

--- MouseMovementBuilder.cs
+++ MouseMovementBuilder.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
 using WindowsInput;
 
 namespace R3peat
 {
     class MouseMovementBuilder
     {
-        private List<MouseMovementStep> MouseMovementSteps;
+        private ObservableCollection<MouseMovementStep> MouseMovementSteps;
         private InputSimulator Input;
         private string Name;
-        private int totalBuiltWithNoName { get; set; }
+        private NameIncrementer NameIncrementer;
         public void BuildMouseMovement() {
-            this.MouseMovementSteps.Clear();
-            this.totalBuiltWithNoName+=1;
-            this.Name = "Mouse Movement "+totalBuiltWithNoName.ToString();
+            this.MouseMovementSteps = new ObservableCollection<MouseMovementStep>();
+            this.Name = NameIncrementer.Next();
         }
         public void BuildMouseMovement(string Name) {
             this.MouseMovementSteps.Clear();
@@ -30,7 +31,8 @@ namespace R3peat
 
         public MouseMovementBuilder(InputSimulator newInput) {
             this.Input = newInput;
-            this.MouseMovementSteps = new List<MouseMovementStep>();
+            this.MouseMovementSteps = new ObservableCollection<MouseMovementStep>();
+            this.NameIncrementer = new NameIncrementer("Mouse Movement");
         }
 
     }
