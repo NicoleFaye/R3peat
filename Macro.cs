4daa361ba840499380b140fa23aa1f5781eaf7e1
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2023-01-06 00:03:14 -0800

    Update Macro.cs

--- Macro.cs
+++ Macro.cs
@@ -1,13 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace R3peat
 {
-    public class Macro : NameAndDescriptionProperty 
+    public class Macro : NameAndDescriptionProperty
     {
-        NameIncrementer NameIncrementer = new NameIncrementer("Macro");
+        ObservableCollection<Action> Actions = new ObservableCollection<Action>();
+        private bool _active;
+        public bool Active
+        {
+            get
+            {
+                return _active;
+            }
+            set
+            {
+                _active = value;
+                base.onPropertyChanged("Active");
+            }
+        }
+        public Macro(string name)
+        {
+            this.Name = name;
+        }
     }
 }
