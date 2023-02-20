9ce7f92e0c681cdd840c56ae3051075d1d86cc5a
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2023-02-20 13:25:44 -0800

    Created Hotkey editor model

---
+++ HotkeyEditorModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace R3peat
+{
+    public class HotkeyEditorModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Macro CurrentMacro;
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        public HotkeyEditorModel(Macro macro) {
+            this.CurrentMacro = macro;
+        
+        }
+    }
+}
