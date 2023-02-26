2551cf0bcbc37ece7401f0433974aeab62e3814f
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2023-02-25 19:27:34 -0800

    formatting

--- HotkeyEditorModel.cs
+++ HotkeyEditorModel.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 using NHotkey.Wpf;
+using System.Windows.Input;
 
 namespace R3peat
 {
@@ -25,6 +26,55 @@ namespace R3peat
         public HotkeyEditorModel(Macro macro, HotkeyManager hotkeyManager) {
             this.CurrentMacro = macro;
         
+        }
+        KeyGesture GetKeyCombo()
+        {
+            KeyGesture keyCombo = null;
+            Array keys = Enum.GetValues(typeof(Key));
+
+            List<Key> shiftKeys = new List<Key>();
+            shiftKeys.Add(Key.LeftShift);
+            shiftKeys.Add(Key.RightShift);
+
+            List<Key> ctrlKeys = new List<Key>();
+            ctrlKeys.Add(Key.LeftCtrl);
+            ctrlKeys.Add(Key.RightCtrl);
+
+            List<Key> altKeys = new List<Key>();
+            altKeys.Add(Key.LeftAlt);
+            altKeys.Add(Key.RightAlt);
+
+            List<Key> modifierKeys = new List<Key>();
+            modifierKeys.Concat(shiftKeys);
+            modifierKeys.Concat(ctrlKeys);
+            modifierKeys.Concat(altKeys);
+
+            foreach (Key k in keys)
+            {
+                if (Keyboard.IsKeyDown(k) && keyCombo == null)
+                {
+                    keyCombo = new KeyGesture(k);
+                }
+                else if (Keyboard.IsKeyDown(k) && keyCombo != null)
+                {
+                    if (modifierKeys.Contains(k) && keyCombo.Key != k)
+                    {
+                        if (altKeys.Contains(k))
+                        {
+                            keyCombo = new KeyGesture(keyCombo.Key, keyCombo.Modifiers | ModifierKeys.Alt);
+                        }
+                        else if (shiftKeys.Contains(k))
+                        {
+                            keyCombo = new KeyGesture(keyCombo.Key, keyCombo.Modifiers | ModifierKeys.Shift);
+                        }
+                        else if (ctrlKeys.Contains(k))
+                        {
+                            keyCombo = new KeyGesture(keyCombo.Key, keyCombo.Modifiers | ModifierKeys.Control);
+                        }
+                    }
+                }
+            }
+            return keyCombo;
         }
     }
 }
--- HotkeyObject.cs
+++ HotkeyObject.cs
@@ -7,29 +7,36 @@ using System.ComponentModel;
 using NHotkey.Wpf;
 using System.Windows.Input;
 using System.Data;
+using Windows.UI.Xaml.Automation.Peers;
+using System.Collections;
 
 namespace R3peat
 {
-    public class HotkeyObject: NameProperty
+    public class HotkeyObject : NameProperty
     {
         private HotkeyManager _manager;
         public HotkeyMode HotkeyMode { get; set; }
         public KeyGesture KeyCombo { get; set; }
         public EventHandler<NHotkey.HotkeyEventArgs> Action { get; set; }
-        public void Update() {
-            if (Action != null) { 
+        public void Update()
+        {
+            if (Action != null)
+            {
                 _manager.AddOrReplace(this.Name, KeyCombo, Action);
             }
         }
-        public HotkeyObject(HotkeyManager hotkeyManager, String name) {
-            _manager= hotkeyManager;
-            this.Name=name;
+        public HotkeyObject(HotkeyManager hotkeyManager, String name)
+        {
+            _manager = hotkeyManager;
+            this.Name = name;
             Action = null;
         }
-        public HotkeyObject(HotkeyManager hotkeyManager,String name,EventHandler<NHotkey.HotkeyEventArgs> handler) {
-            _manager= hotkeyManager;
-            this.Name=name;
+        public HotkeyObject(HotkeyManager hotkeyManager, String name, EventHandler<NHotkey.HotkeyEventArgs> handler)
+        {
+            _manager = hotkeyManager;
+            this.Name = name;
             Action = handler;
         }
+
     }
 }
