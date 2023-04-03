61e09e0f6766c775df7dc121a628fd73d780c347
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2023-04-02 23:00:12 -0700

    Added Key Combination Action

---
+++ src/Actions/KeyCombination.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsInput.Native;
+using WindowsInput;
+
+namespace R3peat
+{
+    public class KeyCombination : Action
+    {
+        private InputSimulator _inputSimulator;
+        public List<VirtualKeyCode> KeyCodes { get; set; }
+
+        public KeyCombination(InputSimulator inputSimulator, string name, List<VirtualKeyCode> keyCodes)
+        {
+            _inputSimulator = inputSimulator;
+            Name = name;
+            KeyCodes = keyCodes;
+        }
+
+        public override void Run()
+        {
+            _inputSimulator.Keyboard.ModifiedKeyStroke(KeyCodes.Take(KeyCodes.Count - 1), KeyCodes.Last());
+        }
+    }
+
+}
