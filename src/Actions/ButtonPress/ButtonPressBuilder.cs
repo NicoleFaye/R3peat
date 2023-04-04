9f290033a60c79878be9e06695a1dc7c5ef8cc3a
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2023-04-03 23:29:15 -0700

    Create ButtonPressBuilder.cs

---
+++ src/Actions/ButtonPress/ButtonPressBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WindowsInput;
+
+namespace R3peat
+{
+
+    public class ButtonPressBuilder
+    {
+        private InputSimulator _inputSimulator;
+        private string _name;
+        private Key _key;
+        private NameIncrementer NameIncrementer;
+
+        public ButtonPressBuilder(InputSimulator inputSimulator)
+        {
+            _inputSimulator = inputSimulator;
+            NameIncrementer = new NameIncrementer("Button Press");
+        }
+
+        public ButtonPressBuilder SetName(string name)
+        {
+            _name = name;
+            return this;
+        }
+
+        public ButtonPressBuilder SetKey(Key key)
+        {
+            _key = key;
+            return this;
+        }
+
+        public ButtonPress Build()
+        {
+            return new ButtonPress(_inputSimulator, _name ?? NameIncrementer.Next(), _key);
+        }
+    }
+
+}
