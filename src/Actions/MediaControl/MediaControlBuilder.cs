ad83aac7a7ccac02045da9e7643a0647a00f55da
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2023-04-03 23:29:12 -0700

    Create MediaControlBuilder.cs

---
+++ src/Actions/MediaControl/MediaControlBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsInput;
+
+namespace R3peat
+{
+
+    public class MediaControlBuilder
+    {
+        private InputSimulator _inputSimulator;
+        private string _name;
+        private MediaControlType _controlType;
+        private NameIncrementer NameIncrementer;
+
+        public MediaControlBuilder(InputSimulator inputSimulator)
+        {
+            _inputSimulator = inputSimulator;
+            NameIncrementer = new NameIncrementer("Media Control");
+        }
+
+        public MediaControlBuilder SetName(string name)
+        {
+            _name = name;
+            return this;
+        }
+
+        public MediaControlBuilder SetControlType(MediaControlType controlType)
+        {
+            _controlType = controlType;
+            return this;
+        }
+
+        public MediaControl Build()
+        {
+            return new MediaControl(_inputSimulator, _name??NameIncrementer.Next(), _controlType);
+        }
+    }
+
+}
