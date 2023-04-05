9bf82cc22f74ed70c22f243771f5aec6f6161b1f
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2023-04-04 20:32:01 -0700

    Added Screenshot Action

---
+++ src/Actions/Screenshot/Screenshot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace R3peat
+{
+    public class Screenshot : Action
+    {
+        public string SavePath { get; set; }
+
+        public Screenshot(string name, string savePath = "" )
+        {
+            Name = name;
+            SavePath = savePath;
+        }
+
+        public override void Run()
+        {
+            using (Bitmap bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+                }
+
+                    bmp.Save(SavePath, ImageFormat.Png);
+            }
+        }
+    }
+}
---
+++ src/Actions/Screenshot/ScreenshotBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R3peat
+{
+    using System.Drawing.Imaging;
+    using System.Windows.Forms;
+
+    public class ScreenshotBuilder
+    {
+        private string _name;
+        private string _savePath;
+        private NameIncrementer NameIncrementer;
+
+        public ScreenshotBuilder()
+        {
+            NameIncrementer = new NameIncrementer("Screenshot");
+        }
+
+        public ScreenshotBuilder SetName(string name)
+        {
+            _name = name;
+            return this;
+        }
+
+        public ScreenshotBuilder SetSavePath(string savePath)
+        {
+            _savePath = savePath;
+            return this;
+        }
+
+        public Screenshot Build()
+        {
+            string finalName = _name ?? NameIncrementer.Next();
+            return new Screenshot(finalName, _savePath);
+        }
+    }
+
+}
